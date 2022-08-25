e1b53820ea4493e85deef0e7dc9a0542267b737f
Soroush Safarkhah <soroush_Raw@hotmail.com>
2022-08-25 10:26:16 +0200

    contact created

--- src/commponents/contact/Contact.jsx
+++ src/commponents/contact/Contact.jsx
@@ -1,7 +1,23 @@
 import "./contact.scss"
+import { useState } from "react";
 
 export default function Contact() {
+  const [message, setMessage] = useState(false);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setMessage(true);
+  };
   return (
-    <div className='contact' id="contact">Contact</div>
-  )
+    <div className="contact" id="contact">
+        <h2>Contact.</h2>
+        <form onSubmit={handleSubmit}>
+          <input type="text" placeholder="Email" />
+          <textarea placeholder="Message"></textarea>
+          <button type="submit">Send</button>
+          {message && <span>Thank you, I will reply to your message :)</span>}
+        </form>
+      
+    </div>
+  );
 }
