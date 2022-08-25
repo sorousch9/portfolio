8f65f02b7b9f782a9a35dbb0c06f6335977a0307
Soroush Safarkhah <soroush_Raw@hotmail.com>
2022-08-25 17:42:57 +0200

    intro updated

--- src/commponents/intro/Intro.jsx
+++ src/commponents/intro/Intro.jsx
@@ -1,28 +1,33 @@
 import "./intro.scss";
 
-
-
 export default function Intro() {
-
   return (
     <div className="intro" id="intro">
       <div className="left">
-        <div className="imgContainer">
-          <img src="assets/man.png" alt="" />
-        </div>
-      </div>
-
-      <div className="right">
         <div className="wrapper">
           <h2>Hallo, ich bin </h2>
-          <h1>Soroush Safar</h1>
-          <h3>
-            WebApp <span>React</span> Entwickler
-          </h3>
+          <h1>Soroush Safar </h1>
         </div>
+      </div>
+      <div className="center">
+        <div className="imgContainer">
+          <img src="assets/loremPhoto.png" alt="" />
+
+        </div>
+      </div>
+      <div className="right">
+        <h2>UI/UX Designer</h2>
+        <h2>Full Stack Web App</h2>
+        <h3>
+          <span>React</span> Entwickler
+        </h3>
+        <h4>
+          Lorem ipsum dolor sit amet consectetu adipisicing elit. Eos obcaecati
+          dolore accusantium nam maiores iure impedit,
+        </h4>
         <a href="#portfolio">
-          <img src="assets/arrow1.png" alt="" />
-        </a>
+            <img src="assets/arrow1.png" alt="" />
+          </a>
       </div>
     </div>
   );
